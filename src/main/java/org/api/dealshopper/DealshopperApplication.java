56ec448268f0bf47734ed0e1e271ef9907c72523
SamuelLazurca <samilazurca15@gmail.com>
2023-04-08 10:52:08 +0300

    Initial project setup done

---
+++ src/main/java/org/api/dealshopper/DealshopperApplication.java
@@ -0,0 +1,13 @@
+package org.api.dealshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealshopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealshopperApplication.class, args);
+	}
+
+}
