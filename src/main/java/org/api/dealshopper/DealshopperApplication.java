323ded03ad89c22c0591efd9ea6b388e4fd7f373
Turcu Mihai <mihaiturcu35m@gmail.com>
2023-05-10 15:21:29 +0300

    Adaugare swagger

--- src/main/java/org/api/dealshopper/DealshopperApplication.java
+++ src/main/java/org/api/dealshopper/DealshopperApplication.java
@@ -1,9 +1,12 @@
 package org.api.dealshopper;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class DealshopperApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DealshopperApplication.class, args);
