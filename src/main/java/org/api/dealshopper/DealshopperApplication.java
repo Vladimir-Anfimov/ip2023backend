3da6e8150a66383629c772c6fa86c3ea7f3479bb
Turcu Mihai <mihaiturcu35m@gmail.com>
2023-05-10 15:32:49 +0300

    Fix swagger

--- src/main/java/org/api/dealshopper/DealshopperApplication.java
+++ src/main/java/org/api/dealshopper/DealshopperApplication.java
@@ -3,7 +3,6 @@ package org.api.dealshopper;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @OpenAPIDefinition
